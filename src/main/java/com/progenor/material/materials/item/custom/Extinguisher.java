16421a5be96b079b175b374df2a4d0243b1a4f6f
progenor <nero6126@gmail.com>
2021-09-09 19:00:56 +0300

    new textures and tooltips

--- src/main/java/com/progenor/material/materials/item/custom/Extinguisher.java
+++ src/main/java/com/progenor/material/materials/item/custom/Extinguisher.java
@@ -1,5 +1,7 @@
 package com.progenor.material.materials.item.custom;
 
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -7,8 +9,12 @@ import net.minecraft.item.ItemUseContext;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.ActionResultType;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 
+import javax.annotation.Nullable;
+import java.util.List;
 import java.util.Objects;
 
 public class Extinguisher extends Item {
@@ -33,6 +39,19 @@ public class Extinguisher extends Item {
             stack.damageItem(1, playerEntity, player -> player.sendBreakAnimation(context.getHand()));
         }
         return super.onItemUseFirst(stack, context);
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+
+        if(Screen.hasShiftDown()){
+            tooltip.add(new TranslationTextComponent("tooltip.materials.extinguisher_shift"));
+        }else {
+            tooltip.add(new TranslationTextComponent("tooltip.materials.extinguisher"));
+        }
+
+
+        super.addInformation(stack, worldIn, tooltip, flagIn);
     }
 
     private void rightClickInAir(PlayerEntity playerEntity) {
