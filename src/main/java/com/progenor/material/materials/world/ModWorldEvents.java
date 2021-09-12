d731df4890fecebde0d542a0261e0061587ab3ef
progenor <nero6126@gmail.com>
2021-09-12 17:37:55 +0300

    tungsten ore generation in world

---
+++ src/main/java/com/progenor/material/materials/world/ModWorldEvents.java
@@ -0,0 +1,17 @@
+package com.progenor.material.materials.world;
+
+import com.progenor.material.materials.Materials;
+import com.progenor.material.materials.world.gen.ModOreGeneration;
+import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Materials.MOD_ID)
+public class ModWorldEvents {
+
+    @SubscribeEvent
+    public static void biomeLoadingEvent(final BiomeLoadingEvent event) {
+        ModOreGeneration.generateOres(event);
+    }
+
+}
